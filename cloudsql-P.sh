23b8de19a7fad18b647f4275f4ae4ac79836459e
vatanji <160595310+vatanji@users.noreply.github.com>
2024-04-26 17:54:10 +0530

    Create cloudsql-P.sh

---
+++ cloudsql-P.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Set variables
+PROJECT_ID="genaiexperiments-1"
+REGION="us-central1"
+INSTANCE_NAME="cloudsqlP-$(date +'%Y%m%d%H%M%S')"
+DATABASE_VERSION="POSTGRES_9_6"
+DATABASE_TIER="db-f1-micro"  # Change the tier to a valid one
+USERNAME="postgres"
+USER_PASSWORD="BinRoot@123"  # Change the password to your desired one
+
+# Create Cloud SQL instance with a valid name
+# Convert uppercase to lowercase and replace non-alphanumeric characters with hyphens
+INSTANCE_NAME=$(echo "$INSTANCE_NAME" | tr '[:upper:]' '[:lower:]' | sed 's/[^a-z0-9]/-/g')
+
+# Create Cloud SQL instance
+gcloud sql instances create $INSTANCE_NAME \
+    --project=$PROJECT_ID \
+    --region=$REGION \
+    --database-version=$DATABASE_VERSION \
+    --tier=$DATABASE_TIER > /dev/null 2>&1  # Redirect output to /dev/null
+
+# Set root password for Cloud SQL instance
+# You might need to have sufficient permissions to execute this command
+gcloud sql users set-password root \
+    --project=$PROJECT_ID \
+    --instance=$INSTANCE_NAME \
+    --password=BinRoot@123 > /dev/null 2>&1  # Redirect output to /dev/null
+
+
+
+# Get public IP address of the PostgreSQL instance
+PUBLIC_IP=$(gcloud sql instances describe $INSTANCE_NAME --project=$PROJECT_ID --format="value(ipAddresses.ipAddress)")
+
+# Print the public IP address
+
+
+
+# Print instance name and external IP address
+echo "Instance_Name: $INSTANCE_NAME "
+echo "External_IP: $PUBLIC_IP"
+
+# Print MySQL database configuration
+echo "MYSQL_DATABASE: "
+echo "MYSQL_USER: postgres"
+echo "MYSQL_PASSWORD: BinRoot@123"
