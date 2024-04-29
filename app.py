d61aa8d5c43e00e57adc204af847df39b6134a9e
vatanji <160595310+vatanji@users.noreply.github.com>
2024-04-30 00:29:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,9 +8,75 @@ CORS(app)
 # Define the authentication token
 AUTH_TOKEN = 'apptoken'
 
-# Path of the shell script to start MySQL
+# Path of the shell script to start services
 START_MYSQL_SCRIPT_PATH = '/home/flask-api/mysql.sh'
 START_PGSQL_SCRIPT_PATH = '/home/flask-api/postgres.sh'
+START_CLOUDSQLM_SCRIPT_PATH = '/home/flask-api/cloudsql-MS.sh'
+START_CLOUDSQLP_SCRIPT_PATH ='/home/flask-api/cloudsql-p.sh'
+START_ALLOYDBOMINI_SCRIPT_PATH = '/home/flask-api/alloyomini.sh'
+
+
+@app.route('/start_alloydbOMINI', methods=['POST'])
+def start_alloydbOMINI():
+    try:
+        # Check if the authentication token is provided
+        auth_token = request.headers.get('Authorization')
+        if auth_token != AUTH_TOKEN:
+            return jsonify({'success': False, 'error': 'Unauthorized access'}), 401
+        print('here', flush=True)
+        script_path = '/home/flask-api/alloyomini.sh'
+        result = execute_shell_script(START_ALLOYDBOMINI_SCRIPT_PATH)
+        insert_result=insert_database(
+        result,sql_instance_type='pgsql')
+        print('here   ', flush=True)
+        if insert_result['success']:
+            return jsonify(result), 200
+        else:
+            return jsonify(result), 500
+    except Exception as e:
+        return jsonify({'success': False, 'error': str(e)}), 500
+
+
+@app.route('/start_cloudsqlP', methods=['POST'])
+def start_cloudsqlP():
+    try:
+        # Check if the authentication token is provided
+        auth_token = request.headers.get('Authorization')
+        if auth_token != AUTH_TOKEN:
+            return jsonify({'success': False, 'error': 'Unauthorized access'}), 401
+        print('here', flush=True)
+        script_path = '/home/flask-api/cloudsql-p.sh '
+        result = execute_shell_script(START_CLOUDSQLP_SCRIPT_PATH)
+        insert_result=insert_database(
+        result,sql_instance_type='pgsql')
+        print('here   ', flush=True)
+        if insert_result['success']:
+            return jsonify(result), 200
+        else:
+            return jsonify(result), 500
+    except Exception as e:
+        return jsonify({'success': False, 'error': str(e)}), 500
+
+
+@app.route('/start_cloudsqlM', methods=['POST'])
+def start_cloudsqlM():
+    try:
+        # Check if the authentication token is provided
+        auth_token = request.headers.get('Authorization')
+        if auth_token != AUTH_TOKEN:
+            return jsonify({'success': False, 'error': 'Unauthorized access'}), 401
+        print('here', flush=True)
+        script_path = '/home/flask-api/cloudsql-MS.sh'
+        result = execute_shell_script(START_CLOUDSQLM_SCRIPT_PATH)
+        insert_result=insert_database(
+        result,sql_instance_type='mysql')
+        print('here   ', flush=True)
+        if insert_result['success']:
+            return jsonify(result), 200
+        else:
+            return jsonify(result), 500
+    except Exception as e:
+        return jsonify({'success': False, 'error': str(e)}), 500
 
 
 @app.route('/start_postgres', methods=['POST'])
