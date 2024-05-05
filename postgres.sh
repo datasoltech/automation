ae7ff402f10b5afa4c34a260e4d6902ea77c4a98
vatanji <160595310+vatanji@users.noreply.github.com>
2024-05-05 16:10:48 +0530

    Update postgres.sh

--- postgres.sh
+++ postgres.sh
@@ -1,31 +1,51 @@
 #!/bin/bash
 
-# Variables
-VM_NAME="my-postgres-$(date +'%Y%m%d%H%M%S')"
+# Generate a timestamp or a random string
+TIMESTAMP=$(date +'%Y%m%d%H%M%S')
+# Or use a random string (requires `uuidgen` command)
+# RANDOM_STRING=$(uuidgen | tr '[:upper:]' '[:lower:]')
+
+# Combine the timestamp/random string with a prefix
+VM_NAME="postgres-$TIMESTAMP"
+# Or use the random string directly
+# VM_NAME="my-debian-12-instance-$RANDOM_STRING"
+
 MACHINE_TYPE="e2-medium"
-REGION="asia-south1-b"
-IMAGE_FAMILY="debian-10"
-
-
-
-
+REGION="us-central1"
+ZONE="${REGION}-a"  # Choose a zone within the region
+IMAGE_PROJECT="debian-cloud"
+IMAGE_FAMILY="debian-12"
 
 # Create VM instance
 gcloud compute instances create $VM_NAME \
     --machine-type=$MACHINE_TYPE \
-    --zone=$REGION \
+    --zone=$ZONE \
     --image-family=$IMAGE_FAMILY \
-    --image-project=debian-cloud
-sleep 20 
+    --image-project=$IMAGE_PROJECT \
+    >/dev/null 2>&1
 
-# SSH into the VM and create Dockerfile
+sleep 25
 
-gcloud compute ssh $VM_NAME --zone=$REGION --command=" sudo apt install -y docker.io"
+# SSH into the VM and install Docker
+gcloud compute ssh $VM_NAME --zone=$ZONE --command="sudo apt update && sudo apt install -y docker.io" >/dev/null 2>&1
 
-gcloud compute ssh $VM_NAME --zone=$REGION --command=" docker run -d \
+# Run PostgreSQL container inside the VM
+gcloud compute ssh $VM_NAME --zone=$ZONE --command="sudo docker run -d \
     --name postgres-db \
     -e POSTGRES_USER=rooot \
     -e POSTGRES_PASSWORD=BinRoot@123 \
     -p 5432:5432 \
-    postgres:latest
- " 
+    postgres:latest" >/dev/null 2>&1
+
+
+# Get external IP address
+EXTERNAL_IP=$(gcloud compute instances describe $VM_NAME --zone=$ZONE --format='get(networkInterfaces[0].accessConfigs[0].natIP)')
+
+# Print instance name and external IP address
+echo "Instance_Name: $VM_NAME"
+echo "External_IP: $EXTERNAL_IP"
+
+# Print MySQL database configuration
+echo "POSTGRES_DATABASE: my_database"
+echo "POSTGRES_USER: rooot"
+echo "POSTGRES_PASSWORD: BinRoot@123"
