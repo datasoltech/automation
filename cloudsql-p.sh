d2fac73c963c5bd99f95a675c75716e3a26ddcb2
vatanji <160595310+vatanji@users.noreply.github.com>
2024-07-01 10:56:43 +0530

    Update cloudsql-p.sh

--- cloudsql-p.sh
+++ cloudsql-p.sh
@@ -25,22 +25,21 @@ nohup gcloud sql instances create $INSTANCE_NAME \
 gcloud sql users set-password root \
     --project=$PROJECT_ID \
     --instance=$INSTANCE_NAME \
-    --password=BinRoot@123 > /dev/null 2>&1  # Redirect output to /dev/null
+    --password=$USER_PASSWORD > /dev/null 2>&1  # Redirect output to /dev/null
 
 sleep 50
 
 # Get public IP address of the PostgreSQL instance
-PUBLIC_IPOUTGOING_IP=$(gcloud sql instances describe $INSTANCE_NAME --project=$PROJECT_ID --format="value(ipAddresses.ipAddress)")
-
-# Print the public IP address
-
-
+PUBLIC_IP=$(gcloud sql instances describe $INSTANCE_NAME --project=$PROJECT_ID --format="value(ipAddresses[0].ipAddress)")
+OUTGOING_IP=$(gcloud sql instances describe $INSTANCE_NAME --project=$PROJECT_ID --format="value(ipAddresses[1].ipAddress)")
 
 # Print instance name and external IP address
-echo "Instance_Name: $INSTANCE_NAME "
-echo "PUBLIC_IP&OUTGOING_IP: $PUBLIC_IPOUTGOING_IP"
+echo "Instance_Name: $INSTANCE_NAME"
+echo "PUBLIC_IP: $PUBLIC_IP"
+echo "OUTGOING_IP: $OUTGOING_IP"
 
-# Print MySQL database configuration
+# Print PostgreSQL database configuration
 echo "POSTGRES_DATABASE: "
 echo "POSTGRES_USER: postgres"
-echo "POSTGRES_PASSWORD: BinRoot@123"
+echo "POSTGRES_PASSWORD: $USER_PASSWORD"
+
